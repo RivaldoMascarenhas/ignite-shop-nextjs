6326de6dbfddc3f446640f477d3d5273b15dbc21
Rivaldo Mascarenhas <rivaldomascarenhas007@gmail.com>
2023-05-19 15:43:12 -0300

    commit 02

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -49,3 +49,12 @@ export default function Home() {
     </HomeContainer>
   );
 }
+export const getServerSideProps = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  return {
+    props: {
+      list: [1, 2, 3],
+    },
+  };
+};
